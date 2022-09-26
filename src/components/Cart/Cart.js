2a3be9b89b03c453242753be76469c8ea3b0ed8a
sourav-mahmud <souravmahmud32@gmail.com>
2022-09-26 19:08:49 +0600

    handle data from local storage to ui

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Cart.css';
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} = props;
+    // console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for(const product of cart){
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+    const tax = total * .1; /*100/10*/
+    const grandTotal = total + shipping + tax;
     return (
-        <div>
+        <div className='cart'>
             <h4>Orders</h4>
-                <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total shipping cost: ${shipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h5>Grand Total: ${grandTotal}</h5>
         </div>
     );
 };
